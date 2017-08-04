c085e2d5ccddd5b03fde63aeb38860e727f356b6
carlosvelaquez <carlosvelaquez@unitec.edu>
2017-08-04 17:02:24 -0600

    Progreso general2

--- Main.cpp
+++ Main.cpp
@@ -2,37 +2,134 @@
 #include <string>
 using namespace std;
 
-char** generarTablero(char**, int);
-void imprimirTablero();
-void atacar(string, char**, int***, int**);
+void imprimirTablero(char[8][8]);
+bool atacar(string, char[8][8], int[4][3][2], int[65][2]);
 string generarNombre(int);
-void bienvenida(string, string, int***, int***);
-int*** posicionesBarcos(int);
-int** llenarMatrizPA();
-char** generarTablero();
+//void bienvenida(string, string, int[4][3][2], int[4][3][2]);
+void bienvenida(string, string);
+//int[4][3][2] posicionarBarcos(int);
+//int* posicionarBarcos(int);
+//int[8][8] llenarMatrizPA();
+//int* llenarMatrizPA();
+//char[8][8] generarTablero();
+//char* generarTablero();
+string seleccionarBebida();
+int verificarBarcosRestantes(int[65][2], int[4][3][2]);
 
 int main(){
 
 	//Arreglos con las posiciones de los barcos
-	int*** posicionesBarcos1;
-	int*** posicionesBarcos2;
-	posicionesBarcos1 = posicionarBarcos(1);
-	posicionesBarcos2 = posicionarBarcos(2);
+	//int posicionesBarcos1[4][3][2];
+	//int posicionesBarcos2[4][3][2];
+	//posicionesBarcos1 = posicionarBarcos(1);
+
+	int tablero1[4][3][2];
+	tablero1[0][0][0] = 1;
+	tablero1[0][0][1] = 1;
+
+	tablero1[0][1][0] = 1;
+	tablero1[0][1][1] = 2;
+
+	tablero1[0][2][0] = 1;
+	tablero1[0][2][1] = 3;
+
+
+	tablero1[1][0][0] = 2;
+	tablero1[1][0][1] = 7;
+
+	tablero1[1][1][0] = 3;
+	tablero1[1][1][1] = 7;
+
+	tablero1[1][2][0] = 4;
+	tablero1[1][2][1] = 7;
+int tablero2[4][3][2];
+
+	tablero1[2][0][0] = 3;
+	tablero1[2][0][1] = 4;
+
+	tablero1[2][1][0] = 4;
+	tablero1[2][1][1] = 4;
+
+	tablero1[2][2][0] = 5;
+	tablero1[2][2][1] = 4;
+
+
+	tablero1[3][0][0] = 7;
+	tablero1[3][0][1] = 1;
+
+	tablero1[3][1][0] = 7;
+	tablero1[3][1][1] = 2;
+
+	tablero1[3][2][0] = 7;
+	tablero1[3][2][1] = 3;
+
+	tablero2[0][0][0] = 1;
+	tablero2[0][0][1] = 1;
+
+	tablero2[0][1][0] = 1;
+	tablero2[0][1][1] = 2;
+
+	tablero2[0][2][0] = 1;
+	tablero2[0][2][1] = 3;
+
+
+	tablero2[1][0][0] = 2;
+	tablero2[1][0][1] = 7;
+
+	tablero2[1][1][0] = 3;
+	tablero2[1][1][1] = 7;
+
+	tablero2[1][2][0] = 4;
+	tablero2[1][2][1] = 7;
+
+
+	tablero2[2][0][0] = 3;
+	tablero2[2][0][1] = 4;
+
+	tablero2[2][1][0] = 4;
+	tablero2[2][1][1] = 4;
+
+	tablero2[2][2][0] = 5;
+	tablero2[2][2][1] = 4;
+
+
+	tablero2[3][0][0] = 7;
+	tablero2[3][0][1] = 1;
+
+	tablero2[3][1][0] = 7;
+	tablero2[3][1][1] = 2;
+
+	tablero2[3][2][0] = 7;
+	tablero2[3][2][1] = 3;
+
+	//int posicionesBarcos1[4][3][2] = tablero1;
+	//int posicionesBarcos2[4][3][2] = tablero2;
+
+	bool hit;
 
 	//Arreglos con las posiciones que han sido atacadas
-	int** posicionesAtacadas1;
-	int** posicionesAtacadas2;
-	posicionesAtacadas1 = llenarMatrizPA();
-	posicionesAtacadas2 = llenarMatrizPA();
+	int posicionesAtacadas1[65][2];
+	int posicionesAtacadas2[65][2];
+
+for(int i = 0; i < 65; i++){
+		for(int j = 0; j < 2; j++){
+			posicionesAtacadas1[i][j] = -1;
+			posicionesAtacadas2[i][j] = -1;
+		}
+	}
 
 
 	//Tableros que se usaran para marcar las posiciones atacadas
-	char** tableroJuego1;
-	char** tableroJuego2;
-	tableroJuego1 = generarTablero();
-	tableroJuego2 = generarTablero();
+	char tableroJuego1[8][8];
+	char tableroJuego2[8][8];	//Booleans para ver si el juego termino y determinar el jugador ganador
 
-	//Booleans para ver si el juego termino y determinar el jugador ganador
+	for(int i = 0; i < 8; i++){
+		for(int j = 0; j < 8; j++){
+			tableroJuego1[i][j] = ' ';
+			tableroJuego2[i][j] = ' ';
+		}
+	}
+
 	bool termino = false;
 	bool gano1 = false;
 	bool gano2 = false;
@@ -44,7 +141,10 @@ int main(){
 	int shotsTomados2 = 0;
 	int turno = 1;
 
-	bienvenida(nombreJugador1, nombreJugador2, posicionesBarcos1, posicionesBarcos2);
+	string nombreJugador1 = "Jugador 1";
+	string nombreJugador2 = "Jugador 2";
+
+	bienvenida(nombreJugador1, nombreJugador2);
 
 	while(!termino){
 		if(turno == 1){
@@ -52,7 +152,7 @@ int main(){
 			cout << "Barcos enemigos restantes: " << barcosRestantes2 << endl
 				<< "Barcos propios restantes: " << barcosRestantes1 << "------------------------------" << endl;
 
-			bool hit = atacar(nombreJugador1, tableroJuego1, posicionesBarcos2);
+			//bool hit = atacar(nombreJugador1, tableroJuego1, tablero2, posicionesAtacadas2);
 			if(hit){
 				cout << nombreJugador2 << " bebe un shot de "<< seleccionarBebida() << endl;
 				shotsTomados2 ++;
@@ -60,7 +160,7 @@ int main(){
 				cout << nombreJugador2 << "está a salvo." << endl;
 			}
 
-			barcosRestantes2 = verificarBarcosRestantes(posicionesAtacadas2, posicionesBarcos2);
+			//barcosRestantes2 = verificarBarcosRestantes(posicionesAtacadas2, tablero2);
 			cout << nombreJugador2 << "tiene " << barcosRestantes2 
 				<< "barcos restantes." << endl;
 		}else{
@@ -68,7 +168,7 @@ int main(){
 			cout << "Barcos enemigos restantes: " << barcosRestantes1 << endl
 				<< "Barcos propios restantes: " << barcosRestantes2 << "------------------------------" << endl;
 
-			bool hit = atacar(nombreJugador2, tableroJuego2, posicionesBarcos1);
+			//bool hit = atacar(nombreJugador2, tableroJuego2, tablero1, posicionesAtacadas1);
 			if(hit){
 				cout << nombreJugador1 << "bebe un shot!" << endl;
 				shotsTomados1 ++;
@@ -76,26 +176,27 @@ int main(){
 				cout << nombreJugador1 << "está a salvo." << endl;
 			}
 
-			barcosRestantes1 = verificarBarcosRestantes(posicionesAtacadas1, posicionesBarcos1);
+			//barcosRestantes1 = verificarBarcosRestantes(posicionesAtacadas1, tablero1);
 			cout << nombreJugador1 << "tiene " << barcosRestantes1 
 				<< "barcos restantes." << endl;
 
 		}
 	}
 
-	if(gano1){
-		terminarJuego(1);
-	}else{
-		terminarJuego(2);
-	}
+	/*if(gano1){
+	  terminarJuego(1);
+	  }else{
+	  terminarJuego(2);
+	  }*/
 
 	return 0;
 
 }
 
-bool atacar(string nombreJugador, char** tablero, int*** posicionesBarcos, int** posicionesAtacadas){
+/*bool atacar(string nombreJugador, char tablero[8][8], int posicionesBarcos[4][3][2], int posicionesAtacadas[4][3][2]){
 	int x, y;
 	bool continuar;
+	bool hit;
 
 	do{
 		continuar = true;
@@ -108,7 +209,7 @@ bool atacar(string nombreJugador, char** tablero, int*** posicionesBarcos, int**
 		cout << "Y: ";
 		cin >> y;
 
-		bool hit = false;
+		hit = false;
 
 		if(x < 0 || x > 7 || y < 0 || y > 7){
 			cout << "[ERROR] Las coordenadas proporionadas superan el tamaño del tablero" << endl;
@@ -149,9 +250,9 @@ bool atacar(string nombreJugador, char** tablero, int*** posicionesBarcos, int**
 
 	cout << "------------------------------" << endl;
 	return hit;
-}
+}*/
 
-int verificarBarcosRestantes(int** posicionesAtacadas, int*** posicionesBarcos){
+/*int verificarBarcosRestantes(int[65][2] posicionesAtacadas, int[4][3][2] posicionesBarcos){
 	int barcos = 0;
 	int hits;
 
@@ -171,13 +272,14 @@ int verificarBarcosRestantes(int** posicionesAtacadas, int*** posicionesBarcos){
 	}
 
 	return barcos;
-}
+}*/
 
-void bienvenida(string nombreJugador1, string nombreJugador2, int*** posicionesBarcos1, int*** posicionesBarcos2){
+//void bienvenida(string nombreJugador1, string nombreJugador2, int *posicionesBarcos1[4][3][2], int *posicionesBarcos2[4][3][2]){
+void bienvenida(string nombreJugador1, string nombreJugador2){
 	cout << "¡Bienvenidos a BATTLESHOTS!" << endl << endl;
-	cout << "Las reglas con simples, es un juego para dos jugadores con el objetivo de dejar a" << endl
-		<< endl << "cualquiera de los jugadores con la menor cantidad de barcos posible. Cada vez que uno de los" << endl
-		<< endl "barcos sea atacado, el jugador atacado deberá de beber un shot que será elegido de manera" << endl
+	cout << "Las reglas con simples, es un juego para dos jugadores con el objetivo de dejar a"
+		<< endl << "cualquiera de los jugadores con la menor cantidad de barcos posible. Cada vez que uno de los"
+		<< endl << "barcos sea atacado, el jugador atacado deberá de beber un shot que será elegido de manera"
 		<< endl << "aleatoria por el sistema. Hay 7 posibilidades de shots en total." << endl << endl;
 
 
@@ -187,17 +289,16 @@ void bienvenida(string nombreJugador1, string nombreJugador2, int*** posicionesB
 	cout << "Ingresa tu nombre: ";
 	cin >> nombreJugador1;
 	cout << "Bienvenido(a), " << nombreJugador1 << ", este es tu tablero: " << endl;
-	mostrarTablero(posicionesBarcos1);
+	//mostrarTablero(posicionesBarcos1);
 	cout << "Presiona ENTER para borrar esta pantalla y continuar" << endl;
 	cin >> a;
 
 	cout << "Jugador 2, voltea la computadora y presiona ENTER" << endl;
-	string a;
 	cin >> a;
 	cout << "Ingresa tu nombre: ";
 	cin >> nombreJugador2;
 	cout << "Bienvenido(a), " << nombreJugador2 << ", este es tu tablero: " << endl;
-	mostrarTablero(posicionesBarcos2);
+	//mostrarTablero(posicionesBarcos2);
 	cout << "Presiona ENTER para borrar esta pantalla y continuar" << endl;
 	cin >> a;
 
@@ -206,30 +307,29 @@ void bienvenida(string nombreJugador1, string nombreJugador2, int*** posicionesB
 string seleccionarBebida(){
 	int codigoBebida;
 
-	switch(codigoBebida){
-		case 1:
-			return "Cheap_tequila";
+	switch(codigoBebida){	case 1:
+		return "Cheap_tequila";
 		case 2:
-			return "Jell-O";
+		return "Jell-O";
 		case 3:
-			return "Kamikaze";
+		return "Kamikaze";
 		case 4:
-			return "MindEraser";
+		return "MindEraser";
 		case 5:
-			return "Bombs";
+		return "Bombs";
 		case 6:
-			return "Fireballs";
+		return "Fireballs";
 		case 7:
-			return "Whiskey";
+		return "Whiskey";
 		default:
-			return "";
+		return "";
 	}
 
 }
 
 
-int*** posicionarBarcos(int jugador){
-	int*** tablero = new int[4][3][3];
+/*int* posicionarBarcos(int jugador){
+	int tablero[4][3][2];
 
 	if(jugador == 1){
 		tablero[0][0][0] = 1;
@@ -311,49 +411,12 @@ int*** posicionarBarcos(int jugador){
 		tablero[3][2][1] = 1;
 	}
 
-	return tablero;
+	int* t = tablero;
+	return t;
 }
-tablero[0][0][0] = 1;
-		tablero[0][0][1] = 1;
 
-		tablero[0][1][0] = 1;
-		tablero[0][1][1] = 2;
-
-		tablero[0][2][0] = 1;
-		tablero[0][2][1] = 3;
-
-
-		tablero[1][0][0] = 2;
-		tablero[1][0][1] = 7;
-
-		tablero[1][1][0] = 3;
-		tablero[1][1][1] = 7;
-
-		tablero[1][2][0] = 4;
-		tablero[1][2][1] = 7;
-
-
-		tablero[2][0][0] = 3;
-		tablero[2][0][1] = 4;
-
-		tablero[2][1][0] = 4;
-		tablero[2][1][1] = 4;
-
-		tablero[2][2][0] = 5;
-		tablero[2][2][1] = 4;
-
-
-		tablero[3][0][0] = 7;
-		tablero[3][0][1] = 1;
-
-		tablero[3][1][0] = 7;
-		tablero[3][1][1] = 2;
-
-		tablero[3][2][0] = 7;
-		tablero[3][2][1] = 3;
-
-int** llenarMatrizPA(){
-	int** matriz = new matriz[65][2];
+int* llenarMatrizPA(){
+	int matriz[65][2];
 
 	for(int i = 0; i < 65; i++){
 		for(int j = 0; j < 2; j++){
@@ -361,11 +424,12 @@ int** llenarMatrizPA(){
 		}
 	}
 
+	//	int* m = matriz;
 	return matriz;
 }
 
-char** generarTablero(){
-	char** tablero = new char[8][8];
+char* generarTablero(){
+	char tablero[8][8];
 
 	for(int i = 0; i < 8; i++){
 		for(int j = 0; j < 8; j++){
@@ -373,5 +437,6 @@ char** generarTablero(){
 		}
 	}
 
+	//	char* t = tablero;
 	return tablero;
-}
+}*/
